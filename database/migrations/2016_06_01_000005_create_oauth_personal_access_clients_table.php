6a2f66d1f995d6c19503ac69a05c5a8bf694df51
dkuzmenchuk <denis.kuzm@tut.by>
2017-02-18 17:42:04 +0300

    Use RethinkDB adapter instead of Eloquent

--- database/migrations/2016_06_01_000005_create_oauth_personal_access_clients_table.php
+++ database/migrations/2016_06_01_000005_create_oauth_personal_access_clients_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+use dkuzmenchuk\RethinkDB\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateOauthPersonalAccessClientsTable extends Migration
--- src/AuthCode.php
+++ src/AuthCode.php
@@ -2,7 +2,7 @@
 
 namespace Laravel\Passport;
 
-use Illuminate\Database\Eloquent\Model;
+use dkuzmenchuk\RethinkDB\Eloquent\Model;
 
 class AuthCode extends Model
 {
